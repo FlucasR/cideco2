c8fa71e64c6aab2deadb758edf65845d344aaa2e
FlucasR <45341957+FlucasR@users.noreply.github.com>
2018-11-25 22:28:40 -0200

    Add files via upload

---
+++ build/precache-manifest.23b94d4ac59326645bfcb49047a1ace2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "fc727a0a6ec7f93af910",
+    "url": "/static/css/main.cee0d81b.chunk.css"
+  },
+  {
+    "revision": "07a97c6cfc75e4e706b0ae7b1cd0e735",
+    "url": "/static/media/bonequinho_ecocidadao_amarelo.07a97c6c.png"
+  },
+  {
+    "revision": "8f82cd30225d287746cd",
+    "url": "/static/js/1.8f82cd30.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc727a0a6ec7f93af910",
+    "url": "/static/js/main.fc727a0a.chunk.js"
+  },
+  {
+    "revision": "fe861e429454006d0bb1da03a6f70714",
+    "url": "/static/media/web_hi_res_512.fe861e42.png"
+  },
+  {
+    "revision": "604cb882f76544fb775e3b3962ddbf9f",
+    "url": "/static/media/bonequinho_ecocidadao_vermelho.604cb882.png"
+  },
+  {
+    "revision": "03da73d450807a498fb826493468cd92",
+    "url": "/static/media/bonequinho_ecocidadao_verde.03da73d4.png"
+  },
+  {
+    "revision": "8f82cd30225d287746cd",
+    "url": "/static/css/1.97a52cca.chunk.css"
+  },
+  {
+    "revision": "1781484e9b3b02fcb0f82a8ed66c237f",
+    "url": "/index.html"
+  }
+];
